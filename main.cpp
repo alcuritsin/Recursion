6d8fd566561bcaf48625ad13eccefefce81de216
alcuritsin <al.curitsin@gmail.com>
2020-12-21 19:56:51 +0500

    init

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+
+void elevator(int floor)
+{
+	if (floor == 0) return;
+
+	std::cout << floor << std::endl;
+	elevator(floor - 1);
+	std::cout << floor << std::endl;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("COLOR 0A");
+	int n =1000000;
+
+	//std::cout << "На каком вы этаже? "; std::cin >> n;
+
+	elevator(n);
+
+
+	/*
+	std::cout << "Hello World!\n";
+
+	std::cout << "Для выхода Esc. Для продолжения любая клавиша.\n";
+
+	if (_getch()!=27) main();
+	*/
+}
