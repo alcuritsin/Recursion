da72f5900964ca6fc3cdb4e6f5e7e7a989d41dd2
alcuritsin <al.curitsin@gmail.com>
2021-01-11 20:18:30 +0500

    TicTacToe --add

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <conio.h>
+
+#define OFFSET_TOP "\n\n\n\n\n\n\n"
+#define OFFSET_LEFT "\t\t\t\t\t\t\t"
+
+void PrintField(char field[], const int n, char player);
+void Move(char field[], const int n, char player);
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	const int n = 9;
+	char field[n]{};
+
+	/*
+	for (int i = 0; i < n; i++)
+	{
+		field[i] = i + 49;
+	}
+	*/
+	PrintField(field, n, 'X');
+}
+
+void PrintField(char field[], const int n, char player)
+{
+	system("CLS");
+
+	std::cout << OFFSET_TOP;
+	std::cout << OFFSET_LEFT << " ";
+	std::cout << "TicTacToe\n\n";
+
+	for (int i = 6; i >= 0; i -= 3)
+	{
+		std::cout << OFFSET_LEFT << " ";
+		
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << field[i + j];
+			if (j!=2) std::cout << " | ";
+		}
+		std::cout << std::endl;
+		std::cout << OFFSET_LEFT;
+		if (i != 0)
+		{
+			std::cout << "--- --- ---";
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << OFFSET_TOP;
+
+	Move(field, n, player);
+}
+
+void Move(char field[], const int n, char player)
+{
+	char key;
+	
+	do
+	{
+		key = _getch();
+
+		if (key == 27)
+		{
+			return;
+		}
+
+		if ((key < '1' || key >'9') && key != 27)
+		{
+			std::cout << "Error!";
+		}
+
+	} while (key < '1' || key >'9');
+
+	//field[key - 49] = key;
+	
+	field[key - 49] = player; //Вносим в массив символ текущего игрока
+
+	(player == 'X') ? player = '0': player = 'X'; //Чередуем символ игрока
+
+	PrintField(field, n, player);
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <conio.h>
 
+#define Fact
+
 void elevator(int floor)
 {
 	if (floor == 0) return;
@@ -10,21 +12,43 @@ void elevator(int floor)
 	std::cout << floor << std::endl;
 }
 
-//int Fact(int digit)
-//{	
-//	int resoult = 1;
-//	if (digit == 0) return;
-//	resoult *=Fact(digit - 1);
-//	
-//	return resoult;
-//}
+int Factorial(int n)
+{	
+	if (n == 0) return 1;
+
+	return n * Factorial(n - 1);
+}
+
+double Power(double a, int n)
+{
+	//if (n == 0) return 1;
+	//
+	//if (n > 0) return a * Power(a, n - 1);
+	//if (n < 0) return 1 / a * Power(a, n - 1);
+	
+	return n>0? a * Power(a, n-1) : n<0 ? 1/a * Power(a, n + 1) : 1;
+}
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
 	system("COLOR 0A");
-	int n = 5;
-	int nf;
+
+
+	//int n = 5;
+
+	//std::cout << "n = "; std::cin >> n;
+	//std::cout << Factorial(n);
+
+	int a;
+	int s;
+
+	std::cout << "a = "; std::cin >> a;
+	std::cout << "n = "; std::cin >> s;
+
+	std::cout << a << "^(" << s << ") = " << Power(a,s);
+
+
 	//std::cout << "На каком вы этаже? "; std::cin >> n;
 
 	// elevator(n);
